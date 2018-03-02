2c1cd192020fb7c7bb1c7af6cc9238afcc296f3d
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-02 21:21:01 +0100

    Modify content script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// var a = 7,
-//	h = 15;
-
 function getTriangleArea(a, h) {
 	if (a > 0) {
+		return a*h/2
 	} else if (h > 0) {
 		return a*h/2;
 	} else {
@@ -10,12 +8,8 @@ function getTriangleArea(a, h) {
 	}
 }
 
-console.log(getTriangleArea(7, 15));
-
-/*
+console.log(getTriangleArea(15, 8));
 
 var triangle1Area = getTriangleArea(10, 15),
 	triangle2Area = getTriangleArea(11, 17),
 	triangle3Area = getTriangleArea(12, 19);
-	
-*/
