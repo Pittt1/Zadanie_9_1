dbb318d34c4e884ab66ef063e662be51422834e5
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-04 09:50:35 +0100

    Change loop in javascript.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 function getTriangleArea(a, h) {
-	if (a > 0) {
-		return a*h/2
-	} else if (h > 0) {
+	if ((a > 0) || (h > 0)) {
 		return a*h/2;
-	} else {
-		return 'Incorrect data';
 	}
 }
 
@@ -13,3 +9,18 @@ console.log(getTriangleArea(15, 8));
 var triangle1Area = getTriangleArea(10, 15),
 	triangle2Area = getTriangleArea(11, 17),
 	triangle3Area = getTriangleArea(12, 19);
+
+var names = ['Asia', 'Kasia', 'Ola', 'Sylwia', 'Ola'];
+var indexOfOla = names.indexOf('Ola');
+
+console.log('The search value is on ' + indexOfOla + ' position');
+
+console.log(names[indexOfOla]);
+
+var values = [1, 2, 3, 4, 5, 6];
+    var multipliedValues = values.map(function(value) {
+    return value * 10;
+});
+
+console.log(values);
+console.log(multipliedValues);
