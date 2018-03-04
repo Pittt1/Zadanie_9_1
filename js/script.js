dd0ea355c99a0ab8be878bd0d5c5fdf353a58336
Piotr Sławek <piotr.slawek@o2.pl>
2018-03-04 12:18:45 +0100

    Change-second return in function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-function getTriangleArea(a, h) {
-	if ((a > 0) || (h > 0)) {
-		return a*h/2;
-	} else {
-		return "Incorrect data";
-	}
-}
+ function getTriangleArea(a, h) {
+  	if ((a > 0) || (h > 0)) {
+  		return a*h/2;
+		} return "Incorrect data";
+  }
 
 console.log(getTriangleArea(15, 8));
 
